f1b66cfe2e1a4e0cb557931f990c3f813ad7f1eb
Tharun B <saitarunbodduluru424@gmail.com>
2023-03-23 15:25:51 -0400

    Initial setup

--- src/main/java/com/restaurant/backend/BackendApplication.java
+++ src/main/java/com/restaurant/backend/BackendApplication.java
@@ -2,8 +2,11 @@ package com.restaurant.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+@EnableMongoRepositories
 public class BackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/restaurant/backend/services/BaseService.java
@@ -0,0 +1,8 @@
+package com.restaurant.backend.services;
+
+import springfox.documentation.swagger2.mappers.ModelMapper;
+
+public class BaseService {
+
+    ModelMapper modelMapper;
+}
